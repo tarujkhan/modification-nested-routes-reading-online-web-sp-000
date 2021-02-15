17a6590bc35bc00bbefdd9ca89e5ac0b808951b7
taroj khan <tarujkhan@yahoo.com>
2021-02-15 20:18:11 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -34,7 +34,7 @@ class PostsController < ApplicationController
 
   def edit
     if params[:author_id]
-
+      author = Author.find_by(id: params[:author_id])
     @post = Post.find(params[:id])
   end
 
