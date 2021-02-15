7ceb2601fad0a3a10babea6deabe3a48c37668e3
taroj khan <tarujkhan@yahoo.com>
2021-02-15 20:25:20 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,6 +17,8 @@ class PostsController < ApplicationController
   end
 
   def new
+    if params[:author_id] && !Author.exists?(params[:author_id])
+      redirect_to authors_path, alert: "Author not found"
     @post = Post.new(author_id: params[:author_id])
   end
 
