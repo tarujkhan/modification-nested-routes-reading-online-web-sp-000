0804dad2ac9817a269a5e50a8255c76875a4cfa0
taroj khan <tarujkhan@yahoo.com>
2021-02-15 20:35:36 +0000

    Automatically backed up by Learn

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,6 +1,6 @@
 module PostsHelper
   def author_id_field(post)
     if post.author.nil?
-      select_tag "post[author_id]"
+      select_tag "post[author_id]", options_from_collection_for_select(Author.all, :id, :name)
 
 end
